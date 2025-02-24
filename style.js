94f89ca9c60e947e6de8c8875e4f45f9eb2e886c
K M Marziul Karim <marziulrafi@gmail.com>
2025-02-24 21:13:45 +0600

    js styles

--- style.js
+++ style.js
@@ -6,3 +6,8 @@ for (const section of sections) {
    section.style.paddingLeft = '10px'
    section.style.backgroundColor = 'antiquewhite'
 }
+
+const placeContainer = document.getElementById('place-container');
+// placeContainer.style.backgroundColor = 'burlywood'
+placeContainer.classList.add('bg')
+placeContainer.classList.remove('large-text')
